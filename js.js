881beab62b1133d4ee8f0a072de0da632c9ef8b8
MakarevichAV <makarevich.a.v@yandex.ru>
2020-11-15 16:32:51 +0300

    slider team done

--- js.js
+++ js.js
@@ -9,7 +9,8 @@ let sliderItemsContainer = document.querySelector('.slider__items'),
     arrowLeft = document.querySelector('.slider__arrow--left'),
     arrowRight = document.querySelector('.slider__arrow--right'),
     slideItems = document.querySelectorAll('.slider__item'),
-    sliderContainer = document.querySelector('.slider');
+    sliderContainer = document.querySelector('.slider'),
+    sliderDots = document.querySelectorAll('.slider__dot');
 
 sliderItemsContainer.style.width = `${slideItems.length + 1}00%`
 
@@ -26,22 +27,32 @@ arrowRight.addEventListener('click', () => {
             arrowRight.click();
         }, 0);
     } else {
-        sliderItemsContainer.style.transition = '0.5s';
+        sliderDots[i].classList.remove('slider__dot--active');
         i++;
-        sliderItemsContainer.style.left = `-${i}00%`;
+        sliderItemsContainer.style.transition = '0.5s';
+        sliderItemsContainer.style.left = `-${i}00%`; 
     }
+    let j = (i === slideItems.length) ? 0 : i;
+    sliderDots[j].classList.add('slider__dot--active');
 });
 arrowLeft.addEventListener('click', () => {
     if (i === 0) {
+        sliderDots[i].classList.remove('slider__dot--active');
         sliderItemsContainer.style.transition = 'none';
         i = slideItems.length;
         sliderItemsContainer.style.left = `-${i}00%`;
+        // sliderDots[i - 1].classList.add('slider__dot--active');
         setTimeout(() => {
             arrowLeft.click();
         }, 0);
     } else {
+        i === slideItems.length ? 
+            sliderDots[i - 1].classList.remove('slider__dot--active') :
+            sliderDots[i].classList.remove('slider__dot--active');
         i--;
         sliderItemsContainer.style.transition = '0.5s';
         sliderItemsContainer.style.left = `-${i}00%`;
     }
+    let j = (i === slideItems.length) ? i - 1 : i;
+    sliderDots[j].classList.add('slider__dot--active');
 });
